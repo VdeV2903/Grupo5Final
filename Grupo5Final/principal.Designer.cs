3b06c2c1e95e61ee3de97c4b7702497c8e1242d9
VdeV2903 <manriquecastor503@gmail.com>
2021-05-16 20:35:42 -0600

    Login Terminado

--- Grupo5Final/principal.Designer.cs
+++ Grupo5Final/principal.Designer.cs
@@ -31,6 +31,7 @@ namespace Grupo5Final
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
@@ -47,11 +48,21 @@ namespace Grupo5Final
             this.panel2.Size = new System.Drawing.Size(626, 405);
             this.panel2.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "principal";
@@ -64,5 +75,6 @@ namespace Grupo5Final
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
     }
 }
