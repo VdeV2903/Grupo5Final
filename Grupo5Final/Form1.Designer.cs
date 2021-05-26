dfa437a9467c0cb6abe8f48262e09f0590afb873
VdeV2903 <manriquecastor503@gmail.com>
2021-05-25 23:15:50 -0600

    ALUMNOS, LIBROS TERMINADO 2

--- Grupo5Final/Form1.Designer.cs
+++ Grupo5Final/Form1.Designer.cs
@@ -67,7 +67,7 @@ namespace Grupo5Final
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(274, 53);
+            this.label1.Location = new System.Drawing.Point(286, 53);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 22);
             this.label1.TabIndex = 1;
